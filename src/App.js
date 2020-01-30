c8c65b892e597b2507f9b01169cf77e3e8064df0
danmuniz86 <danmuniz86@gmail.com>
2020-01-30 15:43:40 -0300

    change the phrase

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code> everything you need </code> and save to reload.
+          This is my first React.js with Amplify
         </p>
         <a
           className="App-link"
